0cf22a2126733726df826094cbb809a8d1d137b7
Job85 <julianjernigan@ymail.com>
2022-12-02 23:54:04 -0500

    began to create form to input new Items

---
+++ src/components/ItemForm.jsx
@@ -0,0 +1,12 @@
+const ItemForm = () => {
+
+    return (
+        <div>
+            <form action="">
+                This is the item form
+            </form>
+        </div>
+    )
+}
+
+export default ItemForm
